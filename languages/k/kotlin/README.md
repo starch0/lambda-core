14ee69a12697c475480e0e4888772dd8069e1813
Potapov Boris <44531453+zxdsc@users.noreply.github.com>
2025-02-04 04:25:35 +0000

    Kotlin implementation

---
+++ languages/k/kotlin/README.md
@@ -0,0 +1,11 @@
+# Kotlin instalation
+Download Kotlin version 2.1.10 ot above from here https://kotlinlang.org/docs/command-line.html
+
+# Run code
+```shell
+# build
+kotlinc lambda-core.kt -include-runtime -d lambda-core.jar
+
+#run
+java -jar lambda-core.jar
+```
---
+++ languages/k/kotlin/lambda-core.kt
@@ -0,0 +1,87 @@
+fun interface T : (T) -> T {}
+// LOGIC    
+val TRUE: T
+   	get() = T {x: T -> T {y: T -> x}}
+val FALSE: T
+   	get() = T {x: T -> T {y: T -> y}}
+val NOT: T 
+   	get() = T {x: T -> x(FALSE)(TRUE)}
+val AND: T
+   	get() = T {x: T -> T {y: T -> x(y)(FALSE)}}
+val OR: T
+   	get() = T {x: T -> T {y: T -> x(TRUE)(y)}}
+
+// CHURCH NUMERALS
+val ZERO: T
+	get() = T{f: T -> T {x: T -> x}}
+val SUCC: T
+	get() = T {n: T -> T {f: T -> T {x: T -> f(n(f)(x))}}}
+val PRED: T
+	get() = T {n: T -> T {f: T -> T {x: T -> n(T {g: T -> T {h: T -> h(g(f))}})(T {u -> x})(T {u -> u})}}}
+
+// HELPERS - not pure lambda calculus
+fun readBool(t: T) {
+	var res: Boolean? = null
+    val _true: T = T {x -> 
+       	res = true
+       	x
+    }
+    val _false: T = T {x -> 
+        res = false
+        x
+    }
+    t(_true)(_false)({t: T -> t})
+    println(res)
+}
+
+fun readChurchNumber(t: T) {
+    var res: Int = 0
+    
+    val plusOne: T = T {x: T ->
+        res++
+        x
+    }
+    
+    t(plusOne)({t:T -> t})
+    println(res)
+}
+
+// -------------------------------------------------
+
+// EXAMPLES
+fun main() {
+    println("LOGIC")
+    println("----------------")
+    println("TRUE/FALSE")
+    readBool(TRUE) // true
+    readBool(FALSE) // false
+
+    println("NOT")
+    readBool(NOT(TRUE)) // false
+    readBool(NOT(FALSE)) //true
+    
+    println("AND")
+    readBool(AND(FALSE)(FALSE)) // false
+    readBool(AND(FALSE)(TRUE)) // false
+    readBool(AND(TRUE)(FALSE)) // false
+    readBool(AND(TRUE)(TRUE)) // false
+    
+    println("OR")
+    readBool(OR(FALSE)(FALSE)) // false
+    readBool(OR(FALSE)(TRUE)) // true
+    readBool(OR(TRUE)(FALSE)) // true
+    readBool(OR(TRUE)(TRUE)) // true
+    
+    println("CHURCH NUMERALS")
+    println("---------------")
+    println("ZERO/SUCC")
+    readChurchNumber(ZERO) // 0
+    readChurchNumber(SUCC(ZERO)) // 1
+    readChurchNumber(SUCC(SUCC(ZERO))) // 2
+    
+    println("PRED")
+    val THREE = SUCC(SUCC(SUCC(ZERO))) // 3
+    readChurchNumber(PRED(THREE)) // 2
+    readChurchNumber(PRED(PRED(THREE))) // 1 
+    readChurchNumber(PRED(PRED(PRED(THREE)))) // 0
+}
